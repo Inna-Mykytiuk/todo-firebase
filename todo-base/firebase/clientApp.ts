b3d1f40b4d208ba6a65abf9048a6a79e13f4193c
Inna Mykytiuk <innka-pinnka@ukr.net>
2024-12-03 18:08:49 +0200

    add rules init firebase

--- todo-base/firebase/clientApp.ts
+++ todo-base/firebase/clientApp.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDwswPSaTUr-h20nKI-CPWdTs7_COT1MmI",
+  authDomain: "firetodo-dd07b.firebaseapp.com",
+  projectId: "firetodo-dd07b",
+  storageBucket: "firetodo-dd07b.firebasestorage.app",
+  messagingSenderId: "160022041715",
+  appId: "1:160022041715:web:481c930b8edb91f2580606",
 };
 
 // Initialize Firebase
